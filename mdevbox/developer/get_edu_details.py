ed908349d84a5fb65e393f0abb81f20a5e7cd821
GodMode <nguruben@gmail.com>
2016-04-17 01:01:51 +0300

    get edu details

--- mdevbox/developer/get_edu_details.py
+++ mdevbox/developer/get_edu_details.py
@@ -6,6 +6,7 @@ class GetEduDetails():
 		edu_details={}
 		edu_details['highest_education']=request_data.get('highest_education')
 		edu_details['institute_name']=request_data.get('institute_name')
+		edu_details['course']=request_data.get('course')
 		edu_details['begin_education']=request_data.get('begin_education')
 		edu_details['end_education']=request_data.get('end_education')
 		return edu_details
