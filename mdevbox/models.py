eed37cb07dd28d9e2b7c8e967052ce3c65bd5111
GodMode <nguruben@gmail.com>
2016-04-18 00:07:30 +0300

    division of table structure

--- mdevbox/models.py
+++ mdevbox/models.py
@@ -17,9 +17,36 @@ class reg_user(models.Model):
     def __unicode__(self):
         return self.user.username
 
-class developers(models.Model):
-    user=models.OneToOneField(User)
+class developers_employment(models.Model):
+    email=models.CharField(max_length=100, unique=True)
     speciality=models.CharField(max_length=100, unique=False)
+    previous_employer=models.CharField(max_length=100, unique=False)
+    role_previous_employment=models.CharField(max_length=100, unique=False)
+    begin_previous_employment=models.DateField(blank=False)
+    end_previous_employment=models.DateField(blank=False)
+    def __unicode__(self):
+        return self.d.email
+
+
+class developers_education(models.Model):
+    email=models.CharField(max_length=100, unique=True)
+    highest_education=models.CharField(max_length=100, unique=False)
+    institute_name=models.CharField(max_length=100, unique=False)
+    begin_education=models.DateField(blank=False)
+    end_education=models.DateField(blank=False)
+    def __unicode__(self):
+    	return self.user.email
+
+class developers_portfolio(models.Model):
     email=models.CharField(max_length=100, unique=False)
-    yoe=models.CharField(max_length=100, unique=False)
+    portfoli_name=models.CharField(max_length=100, unique=False)
+    portfoli_tech=models.CharField(max_length=100, unique=False)
+    portfoli_link=models.CharField(max_length=100, unique=False)
+    portfoli_desc=models.CharField(max_length=100, unique=False)
+    def __unicode__(self):
+        return self.user.email
+
+
+
+
 
