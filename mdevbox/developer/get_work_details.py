7f1081ba95cd08e262f5f1a03a3be2251046d2a9
GodMode <nguruben@gmail.com>
2016-04-20 09:23:10 +0300

    get work details working

--- mdevbox/developer/get_work_details.py
+++ mdevbox/developer/get_work_details.py
@@ -1,14 +1,16 @@
 class GetWorkDetails():
-	def run(data, request_data):
-		data['work_deails]=GetWorkDetails.get_details(request_data)
+	def run(request_data,data):
+		data['work_data']=GetWorkDetails.get_work_data(request_data)
+		#print (request_data)
+
+	def get_work_data(request_data):
+		work_data={}
+		work_data['email']=request_data.get('email')
+		work_data['speciality'] = request_data.get('speciality')
+		work_data['previous_employer'] = request_data.get('previous_employer')
+		work_data['role_previous_employment'] = request_data.get('role_previous_employment')
+		work_data['begin_previous_employment'] = request_data.get('begin_previous_employment')
+		work_data['end_previous_employment']= request_data.get('end_previous_employment')
+		return  work_data
 
 
-	def get_details(request_data):
-		work_deails={}
-		work_deails['email']=request_data.get('email')
-		work_deails['speciality']=request_data.get('speciality')
-		work_deails['previous_employer']=request_data.get('previous_employer')
-		work_deails['role_previous_employment']=request_data.get('role_previous_employment')
-		work_deails['begin_previous_employment']=request_data.get('begin_previous_employment')
-		work_deails['end_previous_employment']=request_data.get('end_previous_employment')
-		return work_deails
