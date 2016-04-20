fed7a38020757c89d546427583e7918ece85817c
GodMode <nguruben@gmail.com>
2016-04-20 09:23:00 +0300

    get portfolio items succesful

--- mdevbox/developer/get_portfolio.py
+++ mdevbox/developer/get_portfolio.py
@@ -1,11 +1,13 @@
 class GetPortfolio():
-	def run(data, request_data):
-		data['portfolio']=GetPortfolio.get_details(request_data)
+	def run(request_data,data):
+		data['portfolio']=GetPortfolio.get_portfolio(request_data)
+		#print (request_data)
 
-	def get_details(request_data)
+	def get_portfolio(request_data):
 		portfolio={}
-		portfolio['portfoli_name']=request_data.get('portfoli_name')
-		portfolio['portfoli_tech']=request_data.get('portfoli_tech')
-		portfolio['portfoli_link']=request_data.get('portfoli_link')
-		portfolio['portfoli_desc']=request_data.get('portfoli_desc')
+		portfolio['portfoli_name'] = request_data.get('portfoli_name')
+		portfolio['portfoli_tech'] = request_data.get('portfoli_tech')
+		portfolio['portfoli_link'] = request_data.get('portfoli_link')
+		portfolio['portfoli_desc'] = request_data.get('portfoli_desc')
 		return portfolio
+
