1ec81eaa343b816d475db9862391336a2e14f061
GodMode <nguruben@gmail.com>
2016-04-20 09:22:45 +0300

    modified fetch working details

--- mdevbox/developer/get_edu_details.py
+++ mdevbox/developer/get_edu_details.py
@@ -1,12 +1,15 @@
 class GetEduDetails():
-	def run(data, request_data):
-		data['edu_details']=GetEduDetails.get_details(request_data)
 
-	def get_details(request_data):
-		edu_details={}
-		edu_details['highest_education']=request_data.get('highest_education')
-		edu_details['institute_name']=request_data.get('institute_name')
-		edu_details['course']=request_data.get('course')
-		edu_details['begin_education']=request_data.get('begin_education')
-		edu_details['end_education']=request_data.get('end_education')
-		return edu_details
+	def run(request_data,data):
+		data['edu_data']=GetEduDetails.get_edu_data(request_data)
+		#print (request_data)
+
+	def get_edu_data(request_data):
+		edu_data={}
+		edu_data['course']=request_data.get('course')
+		edu_data['highest_education']=request_data.get('highest_education')
+		edu_data['institute_name']=request_data.get('institute_name')
+		edu_data['begin_education']=request_data.get('begin_education')
+		edu_data['end_education']=request_data.get('end_education')
+		return  edu_data
+
