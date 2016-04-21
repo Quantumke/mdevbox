ed7a735411656b9627895645f33cd9df30813d1b
GodMode <nguruben@gmail.com>
2016-04-21 12:49:12 +0300

    save who posted the job

--- mdevbox/jobs/apply_job.py
+++ mdevbox/jobs/apply_job.py
@@ -1,6 +1,13 @@
 from mdevbox.models import developers_portfolio
+from mdevbox.models import appliedcandidates
+
 class ApplyJob():
-	def run(data, id):
-		instance=developers_portfolio.objects.get(id=id)
-		email=instance.email
-		print(email)
+	def run(data,request):
+		user=request.user
+		#print(user)
+		instance=developers_portfolio.objects.get(email=user)
+		portfolio_link=instance.portfoli_link
+		applicant=user
+		save=appliedcandidates(applicant=applicant, portfolio_link=portfolio_link)
+		save.save()
+
--- mdevbox/jobs/save_job.py
+++ mdevbox/jobs/save_job.py
@@ -1,12 +1,13 @@
 from mdevbox.models import post_job
 class SaveJob():
-	def run(data):
+	def run(request,data):
 		job_data=data.get('job_data')
 		company=job_data.get('company')
 		job_title=job_data.get('job_title')
 		job_description=job_data.get('job_description')
 		pay=job_data.get('pay')
-		print(job_title, job_description, pay)
-		save=post_job(company=company, job_title=job_title, job_description=job_description, pay=pay)
+		posted_by=request.user
+		#print(job_title, job_description, pay)
+		save=post_job(company=company, job_title=job_title, job_description=job_description, pay=pay, posted_by=posted_by)
 		save.save()
 
